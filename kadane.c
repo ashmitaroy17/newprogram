3a02b72327b261b2686937dbbe25c4a4b17b3e9e
Ashmita Roy <royashmita2020@gmail.com>
2022-10-07 20:44:04 +0530

    initial code

---
+++ kadane.c
@@ -0,0 +1,27 @@
+// Kadane's algo
+
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+    int i, n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i<n; i++)
+    scanf("%d", &arr[i]);
+
+    int max_sum = INT_MIN, curr_sum = 0;
+    for(int i = 0; i<n; i++){
+        curr_sum += arr[i];
+
+        if(max_sum <curr_sum)
+        max_sum = curr_sum;
+
+       if(curr_sum<0)
+        curr_sum = 0;
+
+    }
+    printf("kadanes array = %d\n", max_sum);
+    return 0;
+}
